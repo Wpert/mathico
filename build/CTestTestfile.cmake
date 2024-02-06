c8a796ce2ee2fdd4e21ffeb626fe3fc369a83647
vpert <amorikov95@gmail.com>
2024-02-06 15:04:17 +0300

    remove cmakefiles

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vpert/gitproj/mathico
-# Build directory: /home/vpert/gitproj/mathico/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
