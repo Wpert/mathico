3f5b13c110eada6304f429b6c800b96c0f3f1686
vpert <amorikov95@gmail.com>
2024-02-06 14:58:40 +0300

    -a

---
+++ build/CMakeFiles/Mathico.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vpert/gitproj/mathico/gui/gui.cpp" "CMakeFiles/Mathico.dir/gui/gui.cpp.o" "gcc" "CMakeFiles/Mathico.dir/gui/gui.cpp.o.d"
+  "/home/vpert/gitproj/mathico/gui/page/buttons/tbuttons.cpp" "CMakeFiles/Mathico.dir/gui/page/buttons/tbuttons.cpp.o" "gcc" "CMakeFiles/Mathico.dir/gui/page/buttons/tbuttons.cpp.o.d"
+  "/home/vpert/gitproj/mathico/gui/page/slider/slider.cpp" "CMakeFiles/Mathico.dir/gui/page/slider/slider.cpp.o" "gcc" "CMakeFiles/Mathico.dir/gui/page/slider/slider.cpp.o.d"
+  "/home/vpert/gitproj/mathico/gui/page/textboxes/textbox.cpp" "CMakeFiles/Mathico.dir/gui/page/textboxes/textbox.cpp.o" "gcc" "CMakeFiles/Mathico.dir/gui/page/textboxes/textbox.cpp.o.d"
+  "/home/vpert/gitproj/mathico/gui/page/tpage.cpp" "CMakeFiles/Mathico.dir/gui/page/tpage.cpp.o" "gcc" "CMakeFiles/Mathico.dir/gui/page/tpage.cpp.o.d"
+  "/home/vpert/gitproj/mathico/main.cpp" "CMakeFiles/Mathico.dir/main.cpp.o" "gcc" "CMakeFiles/Mathico.dir/main.cpp.o.d"
+  "/home/vpert/gitproj/mathico/math/polygon.cpp" "CMakeFiles/Mathico.dir/math/polygon.cpp.o" "gcc" "CMakeFiles/Mathico.dir/math/polygon.cpp.o.d"
+  "/home/vpert/gitproj/mathico/math/vmath.cpp" "CMakeFiles/Mathico.dir/math/vmath.cpp.o" "gcc" "CMakeFiles/Mathico.dir/math/vmath.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
